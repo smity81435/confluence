871e9b95bcef03f02cb3f1739deb12c51f0aa528
smity81435 <dsmith@talusanalytics.com>
2018-11-12 14:31:40 -0700

    firebase added

---
+++ js/script.js
@@ -0,0 +1,9 @@
+  var config = {
+    apiKey: "AIzaSyDten3ucSOFU6mGJcXLcmqLMK-xd8vDbT4",
+    authDomain: "confluence-c54ed.firebaseapp.com",
+    databaseURL: "https://confluence-c54ed.firebaseio.com",
+    projectId: "confluence-c54ed",
+    storageBucket: "confluence-c54ed.appspot.com",
+    messagingSenderId: "431697827838"
+  };
+  firebase.initializeApp(config);
